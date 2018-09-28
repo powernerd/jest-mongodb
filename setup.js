cee9782c3263ce62c1dfd3ff3e65c76ec1a1e180
Andy Saman <andys@megameeting.com>
2018-09-28 09:43:00 -0700

    linter changes
    My linter setup made these changes automatically.

--- setup.js
+++ setup.js
@@ -1,21 +1,22 @@
 const path = require('path');
 const fs = require('fs');
 const MongodbMemoryServer = require('mongodb-memory-server');
+
 const globalConfigPath = path.join(__dirname, 'globalConfig.json');
 
 const mongod = new MongodbMemoryServer.default({
   instance: {
-    dbName: 'jest'
+    dbName: 'jest',
   },
   binary: {
-    version: '3.2.18'
-  }
+    version: '3.2.18',
+  },
 });
 
-module.exports = async function() {
+module.exports = async function () {
   const mongoConfig = {
     mongoDBName: 'jest',
-    mongoUri: await mongod.getConnectionString()
+    mongoUri: await mongod.getConnectionString(),
   };
 
   // Write global config to disk because all tests run in different contexts.
